5c6b9f2b86690e6a43d9aff487877c23d63a6372
Samuel Folledo <samuelfolledo@gmail.com>
2021-04-15 22:23:28 -0400

    [add] collection keys

---
+++ Petulia/Constants/CollectionKeys.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionKeys.swift
+//  Petulia
+//
+//  Created by Samuel Folledo on 4/15/21.
+//  Copyright © 2021 Johandre Delgado . All rights reserved.
+//
+
+import Foundation
+import FirebaseFirestore
+import FirebaseAuth
+
+let db = Firestore.firestore()
+let auth = Auth.auth()
+
+enum CollectionKeys {
+  static var users = "Users"
+}
