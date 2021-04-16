b53b18bf8fd4a7b7d38e46313d75508f1c689cef
Samuel Folledo <samuelfolledo@gmail.com>
2021-04-15 21:26:56 -0400

    [add] a way reininitialize root VC

--- Petulia/Services/AppService.swift
+++ Petulia/Services/AppService.swift
@@ -6,4 +6,20 @@
 //  Copyright © 2021 Johandre Delgado . All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+struct AppService {
+    static func initRootView(rootController: UIViewController) {
+        guard let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate else {
+            fatalError("could not get scene delegate ")
+        }
+        sceneDelegate.window?.rootViewController = rootController
+        // A mask of options indicating how you want to perform the animations.
+        let options: UIView.AnimationOptions = .transitionCrossDissolve
+        // Creates a transition animation.
+        UIView.transition(with: sceneDelegate.window!, duration: 0.3, options: options, animations: {}, completion:
+        { completed in
+            // maybe do something on completion here
+        })
+    }
+}
