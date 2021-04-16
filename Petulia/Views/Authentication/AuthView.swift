1f7b018d432eba6a5edcd60980735d29ff853245
Samuel Folledo <samuelfolledo@gmail.com>
2021-04-15 21:28:46 -0400

    [add] made sure auth works

--- Petulia/Views/Authentication/AuthView.swift
+++ Petulia/Views/Authentication/AuthView.swift
@@ -10,9 +10,6 @@ import SwiftUI
 
 struct AuthView: View {
   
-  private let screenWidth = UIScreen.main.bounds.width
-  private let screenHeight = UIScreen.main.bounds.height
-  
   @State var isLogin = false
   @State var isSignup = false
   @State var email: String = ""
@@ -30,10 +27,10 @@ struct AuthView: View {
           signupButton
         }
         Spacer()
-        NavigationLink(destination: LoginView(), isActive: $isLogin, label: {
+        NavigationLink(destination: LoginView(viewModel: AuthenticationViewModel(authType: .login)), isActive: $isLogin, label: {
           EmptyView()
         })
-        NavigationLink(destination: SignupView(), isActive: $isSignup, label: {
+        NavigationLink(destination: SignupView(viewModel: AuthenticationViewModel(authType: .signup)), isActive: $isSignup, label: {
           EmptyView()
         })
       }
@@ -48,7 +45,7 @@ extension AuthView {
   var headingImage: some View {
     Image("PetuliaLogo")
       .resizable()
-      .frame(width: screenHeight/4, height: screenHeight/4, alignment: .center)
+      .frame(width: Constants.screenHeight/4, height: Constants.screenHeight/4, alignment: .center)
   }
   var headingText: some View {
     Text("Welcome to Petulia")
@@ -62,7 +59,7 @@ extension AuthView {
         .foregroundColor(.white)
         .padding()
     })
-    .frame(width: screenWidth - 32, height: 50, alignment: .center)
+    .frame(width: Constants.screenWidth - 32, height: 50, alignment: .center)
     .background(Color.accentColor)
     .border(Color.clear, width: 2)
     .cornerRadius(100)
@@ -70,7 +67,7 @@ extension AuthView {
   var signupButton: some View {
     Button(action: signupButtonTapped, label: {
       Text("Sign up")
-        .frame(width: screenWidth - 32, height: 50, alignment: .center)
+        .frame(width: Constants.screenWidth - 32, height: 50, alignment: .center)
         .font(.system(size: 18, weight: .regular, design: .default))
         .background(Color.clear)
         .overlay(
